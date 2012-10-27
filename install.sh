8abad894ac36fa4318a6565009b9ef665872877e
Techlive Zheng <techlivezheng@gmail.com>
2012-10-27 18:54:03 +0800

    Clean up the code for further development

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 dir=$(dirname $0)
+
 gconfdir=/apps/gnome-terminal/profiles
 
 echo # This makes the prompts easier to follow (as do other random echos below)
@@ -20,9 +21,11 @@ echo
 ###############################
 
 echo "Please select a color scheme:"
-select scheme in light dark; do
+select scheme in light dark
+do
   echo
-  if [[ -z $scheme ]]; then
+  if [[ -z $scheme ]]
+  then
     echo -e "ERROR: Invalid selection -- ABORTING!\n"
     exit 2
   fi
@@ -30,12 +33,12 @@ select scheme in light dark; do
   case $scheme in
     light ) bg_color_file=$dir/colors/base3
             fg_color_file=$dir/colors/base00
-            bold_color_file=$dir/colors/base01
+            bd_color_file=$dir/colors/base01
             ;;
 
     dark  ) bg_color_file=$dir/colors/base03
             fg_color_file=$dir/colors/base0
-            bold_color_file=$dir/colors/base1
+            bd_color_file=$dir/colors/base1
             ;;
   esac
   break
@@ -48,22 +51,22 @@ done
 declare -a profiles
 declare -a visnames
 profiles=($(gconftool-2 -R $gconfdir | grep $gconfdir | cut -d/ -f5 |  cut -d: -f1))
-
-#get visible names
-for index in  ${!profiles[@]}; 
-do    
-    visnames[$index]=$(gconftool-2 -g $gconfdir/${profiles[$index]}/visible_name);
+for index in  ${!profiles[@]}
+do
+    visnames[$index]=$(gconftool-2 -g $gconfdir/${profiles[$index]}/visible_name)
 done
+
 echo "Please select a Gnome Terminal profile:"
 IFS=','
 names="${visnames[*]}"
-select profile_name in $names; 
-do 
-  if [[ -z $profile_name ]]; then
+select profile_name in $names
+do
+  if [[ -z $profile_name ]]
+  then
     echo -e "ERROR: Invalid selection -- ABORTING!\n"
     exit 1
   fi
-  break; 
+  break
 done
 profile_key=$(expr ${REPLY} - 1)
 unset IFS
@@ -75,13 +78,13 @@ unset IFS
 echo    "You have selected:"
 echo    "  Scheme:  $scheme"
 echo -e "  Profile: $profile_name (gconf key: ${profiles[$profile_key]})\n"
-
-#typeset -u confirmation
-echo "Are you sure you want to overwrite the selected profile?"
+echo    "Are you sure you want to overwrite the selected profile?"
 echo -n "(YES to continue) "
+
 read confirmation
 confirmation=$(echo $confirmation | tr '[:lower:]' '[:upper:]')
-if [[ $confirmation != YES ]]; then
+if [[ $confirmation != YES ]]
+then
   echo -e "ERROR: Confirmation failed -- ABORTING!\n"
   exit 3
 fi
@@ -93,16 +96,17 @@ fi
 echo -e "Confirmation received -- applying settings\n"
 
 profile_path=$gconfdir/${profiles[$profile_key]}
-# set palette
+
+# set color palette
 gconftool-2 -s -t string $profile_path/palette $(cat $dir/colors/palette)
 
-# set highlighted color to be different from foreground-color
+# set highlighted color to be different from foreground color
 gconftool-2 -s -t bool $profile_path/bold_color_same_as_fg false
 
-# set foreground to base00 and background to base3 and highlight color to base01
+# set foreground, background and highlight color
 gconftool-2 -s -t string $profile_path/background_color $(cat $bg_color_file)
 gconftool-2 -s -t string $profile_path/foreground_color $(cat $fg_color_file)
-gconftool-2 -s -t string $profile_path/bold_color       $(cat $bold_color_file)
+gconftool-2 -s -t string $profile_path/bold_color       $(cat $bd_color_file)
 
 # make sure the profile is set to not use theme colors
 gconftool-2 -s -t bool $profile_path/use_theme_colors false
